0ef68722b0cebbaa8bf28c4ca562c0968d833b91
ahquan <khoomingquan87@gmail.com>
2025-04-15 14:28:02 +0800

    week 12

---
+++ JAVASCRIPT/function.js
@@ -0,0 +1,38 @@
+//alert('This is external JS');
+
+function addition(){
+    let a = 25;
+    let b = 10;
+
+    console.log(a + b);
+}
+
+function subsition(a , b){
+
+    console.log(a-b);
+}
+
+function multiplication(){
+    let a = 6;
+    let b = 8;
+
+    let result = a * b;
+
+    console.log(result);
+
+}
+
+function division(a , b){
+  
+    console.log(a / b);
+}
+
+function Countdown(){
+    let Countdown = 10;
+
+    while (Countdown > 0){
+        console.log("Countdown " +Countdown);
+        Countdown--;
+    }
+    console.log("SELAMAT HARI RAYA AIDILFITRI");
+}
